d57295def4456f1a7937b8a839a25b5db7cc9b44
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-30 09:46:55 -0500

    add urs API in App

--- EmployeeApp/urls.py
+++ EmployeeApp/urls.py
@@ -4,5 +4,8 @@ from EmployeeApp import views
 
 urlpatterns = [
    url(r'^department$',views.DepartmentApi),
-   url(r'^department/([0-9]+)$', views.DepartmentApi)
+   url(r'^department/([0-9]+)$', views.DepartmentApi),
+
+   url(r'^employee$',views.EmployeeApi),
+   url(r'^employee/([0-9]+)$', views.EmployeeApi),
 ]
