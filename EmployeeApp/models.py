2e984bc9e87526aa62d588c44795ea968101bc18
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-30 03:58:27 -0500

    add  models app at pgsql

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Departments(models.Model):
+    DepartmentID = models.AutoField(primary_key=True)
+    DepartmentName = models.CharField(max_length=500)
+
+class Employees(models.Model):
+    EmployeeID = models.AutoField(primary_key=True)
+    EmployeesName = models.CharField(max_length=500)
+    Department = models.CharField(max_length=500)
+    DateofJoining = models.DateField()
+    photoFileName =   models.CharField(max_length=500)
+
+    
+    
+
