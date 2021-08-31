4e4a9c4858b10970b11fd1198c2b63e1d81e3e13
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-08-31 00:19:30 -0500

    add views method SaveFile in API

--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -7,6 +7,7 @@ from django.http.response import JsonResponse
 from EmployeeApp.models import Employees,Departments
 from EmployeeApp.serializers import EmployeeSerializer, DepartmentSerializer
 
+from django.core.files.storage import default_storage
 # Create your views here.
 
 
@@ -14,22 +15,22 @@ from EmployeeApp.serializers import EmployeeSerializer, DepartmentSerializer
 def DepartmentApi(request, id=0):
     if request.method == 'GET':
         departments  = Departments.objects.all()
-        department_serealizer = DepartmentSerializer(departments,
+        department_serializer = DepartmentSerializer(departments,
         many=True)
-        return JsonResponse(department_serealizer.data, safe=False)
+        return JsonResponse(department_serializer.data, safe=False)
     elif request.method == 'POST':
         department_data = JSONParser().parse(request)
-        department_serealizer = DepartmentSerializer(data=department_data)
-        if department_serealizer.is_valid():
-            department_serealizer.save()
+        department_serializer = DepartmentSerializer(data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
             return JsonResponse('Add successfully!', safe=False)
         return JsonResponse('Add failed!', safe=False)    
     elif request.method == 'PUT':
         department_data = JSONParser().parse(request)
         department = Departments.objects.get(DepartmentID=department_data['DepartmentID'])
-        department_serealizer = DepartmentSerializer(department, data=department_data)
-        if department_serealizer.is_valid():
-            department_serealizer.save()
+        department_serializer = DepartmentSerializer(department, data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
             return JsonResponse('Update successfully!', safe=False)
         return JsonResponse('Update failed!', safe=False)
     elif request.method == 'DELETE':
@@ -42,22 +43,22 @@ def DepartmentApi(request, id=0):
 def EmployeeApi(request, id=0):
     if request.method == 'GET':
         employee  = Employees.objects.all()
-        employee_serealizer = EmployeeSerializer(employee,
+        employee_serializer = EmployeeSerializer(employee,
         many=True)
-        return JsonResponse(employee_serealizer.data, safe=False)
+        return JsonResponse(employee_serializer.data, safe=False)
     elif request.method == 'POST':
         employee_data = JSONParser().parse(request)
-        employee_serealizer = EmployeeSerializer(data=employee_data)
-        if employee_serealizer.is_valid():
-            employee_serealizer.save()
+        employee_serializer = EmployeeSerializer(data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
             return JsonResponse('Add successfully!', safe=False)
         return JsonResponse('Add failed!', safe=False)    
     elif request.method == 'PUT':
         employee_data = JSONParser().parse(request)
         employee = Employees.objects.get(EmployeeID=employee_data['EmployeeID'])
-        employee_serealizer = EmployeeSerializer(employee, data=employee_data)
-        if employee_serealizer.is_valid():
-            employee_serealizer.save()
+        employee_serializer = EmployeeSerializer(employee, data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
             return JsonResponse('Update successfully!', safe=False)
         return JsonResponse('Update failed!', safe=False)
     elif request.method == 'DELETE':
@@ -65,3 +66,9 @@ def EmployeeApi(request, id=0):
         employee.delete() 
         return JsonResponse('Delete successfully!',safe=False)
         
+@csrf_exempt
+def SaveFile(request):
+    file = request.FILES['file']
+    file_name = default_storage.save(file.name, file)
+    return JsonResponse('Save file successfully!', safe=False)
+
