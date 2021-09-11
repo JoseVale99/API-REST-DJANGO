5b50b973ea0452dbb3ce22ebd3997d1a2d77083d
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-11 03:09:12 -0500

    add fix path

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -49,12 +49,7 @@ INSTALLED_APPS = [
     'cloudinary'
 ]
 # adding config
-cloudinary.config( 
-  cloud_name = "maximaseguridadapp", 
-  api_key = "454862721894482", 
-  api_secret = "ShnLOkQ0pJCOFfxNysO0puE_C3o",
-  secure = True
-)
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
